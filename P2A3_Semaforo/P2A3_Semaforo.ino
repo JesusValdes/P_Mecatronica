aa05421d22fc42271bb95bef7822d0140effb2a0
JesusValdes <60162296+JesusValdes@users.noreply.github.com>
2022-08-23 21:50:45 -0500

    Update P2A3_Semaforo.ino

--- P2A3_Semaforo/P2A3_Semaforo.ino
+++ P2A3_Semaforo/P2A3_Semaforo.ino
@@ -1,3 +1,4 @@
+//definicion
 #define R1 8
 #define Y1 9
 #define G1 10
@@ -5,32 +6,33 @@
 #define Y2 12
 #define G2 13
 
+//definimos el comportamiento
 void setup() {
-  pinMode(R1,OUTPUT);
-  pinMode(Y1,OUTPUT);
-  pinMode(G1,OUTPUT);
-  pinMode(R2,OUTPUT);
-  pinMode(Y2,OUTPUT);
-  pinMode(G2,OUTPUT);
+  pinMode(R1,OUTPUT); //salida
+  pinMode(Y1,OUTPUT); //salida
+  pinMode(G1,OUTPUT); //salida
+  pinMode(R2,OUTPUT); //salida
+  pinMode(Y2,OUTPUT); //salida
+  pinMode(G2,OUTPUT); //salida
 }// setup
 
 void loop() {
-  digitalWrite(Y1,LOW);
-  digitalWrite(R2,LOW);
-  digitalWrite(R1,HIGH);
-  digitalWrite(G2,HIGH);
-  delay(5000);
-  digitalWrite(G2,LOW);
-  digitalWrite(Y2,HIGH);
-  delay(1000);
-  digitalWrite(Y2,LOW);
-  digitalWrite(R2,HIGH);
-  digitalWrite(R1,LOW);
-  digitalWrite(G1,HIGH);
-  delay(5000);
-  digitalWrite(G1,LOW);
-  digitalWrite(Y1,HIGH);
-  delay(1000);
+  digitalWrite(Y1,LOW); //valor bajo
+  digitalWrite(R2,LOW); //valor bajo
+  digitalWrite(R1,HIGH); // valor alto
+  digitalWrite(G2,HIGH); // valor alto
+  delay(5000); //tiempo prendido
+  digitalWrite(G2,LOW); //bajo
+  digitalWrite(Y2,HIGH); //alto
+  delay(1000); //tiempo
+  digitalWrite(Y2,LOW); //bajo
+  digitalWrite(R2,HIGH); //alto
+  digitalWrite(R1,LOW); //bajo
+  digitalWrite(G1,HIGH); // alto
+  delay(5000); //tiempo
+  digitalWrite(G1,LOW); //bajo
+  digitalWrite(Y1,HIGH); //alto
+  delay(1000); //tiempo 
   
   
 }// loop

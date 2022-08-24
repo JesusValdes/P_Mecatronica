5e4a684125196422bb1a58c0465ee3a15ea4e7b1
JesusValdes <60162296+JesusValdes@users.noreply.github.com>
2022-08-23 21:47:22 -0500

    Update P2A1-Blink.ino

--- P2A1-Blink/P2A1-Blink.ino
+++ P2A1-Blink/P2A1-Blink.ino
@@ -1,9 +1,9 @@
-#define LED 13
+#define LED 13   //definimos el led
 
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode (LED,OUTPUT );
+  pinMode (LED,OUTPUT );  //le decimos que queremos un output
  
 
 
@@ -12,10 +12,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite (LED,HIGH);
-  delay(1000);
-  digitalWrite (LED, LOW);
-  delay(1000);
+  digitalWrite (LED,HIGH);  //valor alto
+  delay(1000);  //tiempo 
+  digitalWrite (LED, LOW);  //valor bajo 
+  delay(1000);  // tiempo
 
 
 
